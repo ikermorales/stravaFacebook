ebc046dac2df351375318e5e64b126f63a4f24c8
Iker Morales <ikermorales@opendeusto.es>
2021-11-28 14:15:32 +0100

    ES INCREIBLE YA ESTÁ!! ODIO LOS SOCKETS

--- stravaFacebook/src/es/deusto/ingenieria/sd/translation/server/FacebookService.java
+++ stravaFacebook/src/es/deusto/ingenieria/sd/translation/server/FacebookService.java
@@ -3,12 +3,14 @@ package es.deusto.ingenieria.sd.translation.server;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class FacebookService extends Thread {
 	private DataInputStream in;
 	private DataOutputStream out;
 	private Socket tcpSocket;
+	private HashMap<String, String> hashFacebook = new HashMap<>();
 
 	private static String DELIMITER = "#";
 	
@@ -18,51 +20,58 @@ public class FacebookService extends Thread {
 		    this.in = new DataInputStream(socket.getInputStream());
 			this.out = new DataOutputStream(socket.getOutputStream());
 			this.start();
+			
+			//Gente que usa Facebook
+			hashFacebook.put("astro@hotmail.com", "nauta");
+			
 		} catch (Exception e) {
-			System.err.println("# TranslationService - TCPConnection IO error:" + e.getMessage());
+			System.err.println("# FacebookService - TCPConnection IO error:" + e.getMessage());
 		}
 	}
 
 	public void run() {
 		try {
 			String data = this.in.readUTF();			
-			System.out.println("   - TranslationService - Received data from '" + tcpSocket.getInetAddress().getHostAddress() + ":" + tcpSocket.getPort() + "' -> '" + data + "'");					
-			data = this.translate(data);
+			System.out.println("   - FacebookService - Received data from '" + tcpSocket.getInetAddress().getHostAddress() + ":" + tcpSocket.getPort() + "' -> '" + data + "'");					
+			data = this.checkUsuarioFacebook(data);
 			this.out.writeUTF(data);					
-			System.out.println("   - TranslationService - Sent data to '" + tcpSocket.getInetAddress().getHostAddress() + ":" + tcpSocket.getPort() + "' -> '" + data.toUpperCase() + "'");
+			System.out.println("   - FacebookService - Sent data to '" + tcpSocket.getInetAddress().getHostAddress() + ":" + tcpSocket.getPort() + "' -> '" + data.toUpperCase() + "'");
 		} catch (Exception e) {
-			System.out.println("   # TranslationService - TCPConnection error" + e.getMessage());
+			System.out.println("   # FacebookService - TCPConnection error" + e.getMessage());
 		} finally {
 			try {
 				tcpSocket.close();
 			} catch (Exception e) {
-				System.out.println("   # TranslationService - TCPConnection IO error:" + e.getMessage());
+				System.out.println("   # FacebookService - TCPConnection IO error:" + e.getMessage());
 			}
 		}
 	}
-	
-	public String translate(String msg) {
+
+	public String checkUsuarioFacebook(String msg) { 
 		String translation = null;
 		
 		if (msg != null && !msg.trim().isEmpty()) {
 			try {
 				StringTokenizer tokenizer = new StringTokenizer(msg, DELIMITER);		
-				String langFrom = tokenizer.nextToken();
-				String langTo = tokenizer.nextToken();
-				String text = tokenizer.nextToken();
-				System.out.println("   - Starting translation of " + text + " from: " + langFrom + " to " + langTo);
+				String email = tokenizer.nextToken();
+				String contrasenya = tokenizer.nextToken();
+				System.out.println("   Starting checking of " + email + " from: " + contrasenya);
 		
-				if (langFrom != null && langTo != null && text != null && !text.trim().isEmpty()) {
-					RegistrationFacebook gt = new RegistrationFacebook();
-					translation = gt.translate(langFrom, langTo, text);
-					System.out.println("   - Google Translator result: " + translation);
+				if (email != null && contrasenya != null) {
+					if(hashFacebook.containsKey(email)) {
+						if(hashFacebook.get(email).matches(contrasenya)) {
+							return "true";
+						}
+					}
+					
+					System.out.println("   - Facebook server result: " + translation);
 				}
 			} catch (Exception e) {
-				System.out.println("   # TranslationService - Translation API error:" + e.getMessage());
+				System.out.println("   # FacebookService - Facebook API error:" + e.getMessage());
 				translation = null;
 			}
 		}
 		
-		return (translation == null) ? "ERR" : "OK" + DELIMITER + translation;
+		return "false";
 	}
 }
